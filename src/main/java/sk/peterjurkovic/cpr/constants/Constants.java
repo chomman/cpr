b7a96a4beca40af8108a2da11572d101d2e1edb9
peto <peto@peto-HA65M-D2H-B3.(none)>
2013-01-05 17:39:19 +0100

    EH CACHE integrated

--- src/main/java/sk/peterjurkovic/cpr/constants/Constants.java
+++ src/main/java/sk/peterjurkovic/cpr/constants/Constants.java
@@ -2,14 +2,70 @@ package sk.peterjurkovic.cpr.constants;
 
 public class Constants {
 	
+	/**
+	 * Prefix administracie 
+	 * 
+	 * http://localhost:8080/cpr/<strong>admin</strong>/
+	 */
 	public static final String ADMIN_PREFIX = "admin";
 	
+	
+	
+	
+	/** SUCCESS_ROLE_ADMIN_URL
+	 * 
+	 * Rrelativna adresa, na ktoru bude po uspesnom prihlaseni presmerovany
+	 * uzivatel, ktory ma uzivatelsku rolu "ROLE_ADMIN", "ROLE_SUPERADMIN"
+	 */
 	public static final String SUCCESS_ROLE_ADMIN_URL = "/admin/";
+	
+	
+	
+	
+	/** SUCCESS_ROLE_USER_URL
+	 * 
+	 * Relativna adresa, na ktoru bude po uspesnom prihlaseni presmerovany
+	 * uzivatel, ktory ma uzivatelsku rolu "ROLE_USER"
+	 */
 	public static final String SUCCESS_ROLE_USER_URL = "/";
 	
+	
+	
+	
+	/** FAILURE_ROLE_USER_URL
+	 * 
+	 * Relativna adresa, na ktoru bude uzivatel presmerovany po neuspesnom prihlaseni
+	 * v pripade ak sa prihlasoval uzivatelskej prihlasovacej stranky definovanej v "USER_ENTRY_POIN_REDIRECT_URL"
+	 */
 	public static final String FAILURE_ROLE_USER_URL = "/login?login_error=1";
+	
+	
+	
+	
+	/** FAILURE_ROLE_ADMIN_URL
+	 * 
+	 * Relativna adresa, na ktoru bude uzivatel presmerovani po neuspesnom prihlaseni, 
+	 * v pripade ak sa prihlasoval prostrednictvom administracneho prihlasovacieho formulara,
+	 * Definovaneho v "ADMIN_ENTRY_POIN_REDIRECT_URL" 
+	 */
 	public static final String FAILURE_ROLE_ADMIN_URL = "/admin/login?login_error=1";
 	
+	
+	
+	
+	
+	/** ADMIN_ENTRY_POIN_REDIRECT_URL
+	 * 
+	 * 
+	 */
 	public static final String ADMIN_ENTRY_POIN_REDIRECT_URL = "/admin/login";
+	
+	
+	
+	
+	/** USER_ENTRY_POIN_REDIRECT_URL
+	 * 
+	 * 
+	 */
 	public static final String USER_ENTRY_POIN_REDIRECT_URL = "/login";
 }
